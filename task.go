7f3e0a1a0e0af65e24690024a3acc5b25fab336b
nima <nima.kdz13@gmail.com>
2026-08-07 17:35:21 +0330

    Fix list command

--- task.go
+++ task.go
@@ -20,21 +20,22 @@ func AddTask(description string) {
 		return
 	}
 	newTask := Task{
-		ID: len(tasks) + 1,
+		ID:          len(tasks) + 1,
 		Description: description,
-		Status: "Todo",
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
+		Status:      "Todo",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
 	}
 	tasks = append(tasks, newTask)
 	err = SaveTasks(tasks)
 	if err != nil {
 		fmt.Println(err)
 	}
-	
+
 }
 
 func ListTasks() {
+	Loadtasks()
 	for _, task := range tasks {
 		fmt.Printf("%+v\n", task)
 	}
