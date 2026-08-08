34894b3cf8663a185d7d4fbea3180d6c225aa6a2
nima <nima.kdz13@gmail.com>
2026-08-08 21:19:29 +0330

    Write Func Delete

--- task.go
+++ task.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
 
@@ -19,8 +20,14 @@ func AddTask(description string) {
 		fmt.Println(err)
 		return
 	}
+	maxID := 0
+	for _, task := range tasks {
+		if task.ID > maxID {
+			maxID = task.ID
+		}
+	}
 	newTask := Task{
-		ID:          len(tasks) + 1,
+		ID:          maxID + 1,
 		Description: description,
 		Status:      "Todo",
 		CreatedAt:   time.Now(),
@@ -31,7 +38,7 @@ func AddTask(description string) {
 	if err != nil {
 		fmt.Println(err)
 	}
-
+	fmt.Println("✅ Task Added.")
 }
 
 func ListTasks() {
@@ -41,6 +48,39 @@ func ListTasks() {
 		return
 	}
 	for _, task := range tasks {
-		fmt.Printf("ID: %d | %s | %s\n", task.ID, task.Description, task.Status)
+		fmt.Printf("ID: %d | Description: %s | Status: %s\n", task.ID, task.Description, task.Status)
 	}
+}
+
+func DeleteTask(id string) {
+	idStr := id
+	idToDelete, err := strconv.Atoi(idStr)
+	if err != nil {
+		fmt.Println("❌ ID Should a Int: ", err)
+		return
+	}
+	tasks, err := Loadtasks()
+	if err != nil {
+		fmt.Println("❌ Error: ", err)
+		return
+	}
+	newTasks := make([]Task, 0, len(tasks))
+	found := false
+	for _, task := range tasks {
+		if task.ID == idToDelete {
+			found = true
+			continue
+		}
+		newTasks = append(newTasks, task)
+	}
+	if !found {
+		fmt.Println("❌ ID Not Found.")
+		return
+	}
+	err = SaveTasks(newTasks)
+	if err != nil {
+		fmt.Println("❌ Error: ", err)
+		return
+	}
+	fmt.Println("✅ Task Deleted.")
 }
