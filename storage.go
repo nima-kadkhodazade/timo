205cef3f2990981a1c04f113375e0c79610a12e4
nima <nima.kdz13@gmail.com>
2026-08-14 19:26:16 +0330

    Fix pathFile for excutable timo

--- storage.go
+++ storage.go
@@ -3,13 +3,22 @@ package main
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
 )
 
 var tasks []Task
 
 func Loadtasks() ([]Task, error) {
-	data, err := os.ReadFile("tasks.json")
+	path, err := TasksFilePath()
 	if err != nil {
+		return nil, err
+	}
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []Task{}, nil
+		}
 		return nil, err
 	}
 	err = json.Unmarshal(data, &tasks)
@@ -20,13 +29,36 @@ func Loadtasks() ([]Task, error) {
 }
 
 func SaveTasks(task []Task) error {
-	data, err := json.MarshalIndent(task, " ", " ")
+	data, err := json.MarshalIndent(task, "", "  ")
 	if err != nil {
 		return err
 	}
-	err = os.WriteFile("tasks.json", data, 0644)
+
+	path, err := TasksFilePath()
 	if err != nil {
 		return err
 	}
+
+	err = os.WriteFile(path, data, 0644)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func TasksFilePath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	dataDir := filepath.Join(home, ".local", "share", "timo")
+
+	err = os.MkdirAll(dataDir, 0755)
+	if err != nil {
+		return "", err
+	}
+
+	return filepath.Join(dataDir, "tasks.json"), nil
 }
